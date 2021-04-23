6d181d57a3cfe61d9378a1a05a6ade8732f23720
Branden Adams <Branden.Adams@colorado.edu>
2021-04-23 12:51:41 -0600

    First attempt to put polynav and Lab4_2 together

--- Lab4_2.py
+++ Lab4_2.py
@@ -32,9 +32,11 @@ def empty(a):
 	pass
 
 cv2.namedWindow("Parameters")
-cv2.resizeWindow("Parameters", 640,240)
-cv2.namedWindow("Thresholds")
-cv2.resizeWindow("Thresholds", 640,240)
+cv2.resizeWindow("Parameters",800,600)
+cv2.createTrackbar("Thresh 1","Parameters",43,255,empty)
+cv2.createTrackbar("Thresh 2","Parameters",20,255,empty)
+cv2.createTrackbar("Threshold1","Parameters",11,20,empty)
+cv2.createTrackbar("Threshold2","Parameters",2,10,empty)
 
 #Generates the slider bar window with values pre-set for the Red balloon
 #as of now
@@ -44,12 +46,14 @@ cv2.createTrackbar("V_HIGH ","Parameters", 255,255,empty)
 cv2.createTrackbar("H_LOW ", "Parameters", 0, 255, empty)
 cv2.createTrackbar("S_LOW ", "Parameters", 117,255,empty)
 cv2.createTrackbar("V_LOW ","Parameters", 151, 255,empty)
-#cv2.createTrackbar("T1: ","Thresholds")
-#cv2.createTrackbar("T2: ","Thresholds")
+cv2.createTrackbar("Frame Ratio","Parameters",6,10,empty)
+cv2.createTrackbar("Sensetivity","Parameters", 4,10,empty)
+cv2.createTrackbar("Area Min","Parameters",1000,30000,empty)
+cv2.createTrackbar("Area Max","Parameters",5000,100000,empty)
+ROI_FRAME_RATIO = cv2.getTrackbarPos("Frame Ratio", "Parameters")/10
 
-
-
-#cv2.setMouseCallback(frame)
+ROWS = int(frameHeight * ROI_FRAME_RATIO)
+COLS = int(frameWidth)
 
 #Function to draw contours based on the image and parameters provided
 def getContours(img,imgContour):
@@ -68,7 +72,98 @@ def getContours(img,imgContour):
 			cv2.putText(imgContour, "Points: " + str(len(approx)),(x + w + 20, y + 20),cv2.FONT_HERSHEY_COMPLEX, .7, (0,0,255),2)
 			cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),2)
 			b = True
-			#return b
+
+def crop(img):
+    IMAGE_Y_INDEX = 1
+    IMAGE_X_INDEX = 0
+
+    roi_vector = {'x' : 0, 'y' : 0, 'w' : 0, 'h': 0}
+    roi_vector['w'] = img.shape[IMAGE_X_INDEX]
+    roi_vector['h'] = int(img.shape[IMAGE_Y_INDEX] * cv2.getTrackbarPos("Frame Ratio","Parameters")/10)
+    roi_vector['x'] = 0
+    roi_vector['y'] = img.shape[IMAGE_Y_INDEX] - roi_vector['h']
+    y_start = roi_vector['y']
+    y_end = roi_vector['y'] + roi_vector['h']
+    x_start = roi_vector['x']
+    x_end = roi_vector['x'] + roi_vector['w']
+    cropped = img[y_start:y_end, x_start:x_end]
+    h = abs(y_start - y_end)
+    w = abs(x_start - x_end)
+    return cropped, h, w
+
+def polygon(ROWS,COLS,SENS):
+    poly_points = np.array([[int(COLS * 0.25), ROWS],
+        [int (COLS * 0.75), ROWS],
+        [COLS, int(ROWS * SENS)],
+        [COLS, int(ROWS * 0.25)],
+        [0,int(ROWS * 0.25)],
+        [0,int(ROWS * SENS)]], np.int32)
+    return poly_points
+
+def superimpose(img,points):
+    cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    cv2.polylines(img,[points],True,(0,0,0),5)
+    return img
+
+def poly_cont(img):
+
+	bin_image = cv2.adaptiveThreshold(img, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,cv2.getTrackbarPos("Threshold1","Parameters"),cv2.getTrackbarPos("Threshold2","Parameters"))
+
+
+
+	    #ret,bin_image = cv2.threshold(img,cv2.getTrackbarPos("BW_THRESH","Parameters"), 255,cv2.THRESH_BINARY)
+    kernel = np.ones((3,3),np.uint8)
+    #bin_image = cv2.dilate(bin_image, kernel, iterations = 1)
+    bin_image = cv2.erode(bin_image, kernel, iterations = 1)
+    #bin_image = ~bin_image
+    cv2.imshow("Binary",bin_image)
+    contours, hierarchy = cv2.findContours(bin_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    #Might want to put the ConvexHull here
+    return contours, hierarchy
+
+def findpath(img,imgContour,points,contours,Vis):
+    angle_deviation = 0
+    distance_unobstructed = 0
+    area_unobstructed = 0
+    MIN_CONTOUR_AREA = cv2.getTrackbarPos("Area Min","Parameters")
+    MAX_CONTOUR_AREA = cv2.getTrackbarPos("Area Max","Parameters")
+    rows,cols = 600 ,800
+
+    if contours is None:
+        return -1, -1, -1
+
+    y_list = []
+    cnt_list = []
+    ROWS = int(frameHeight * cv2.getTrackbarPos("Frame Ratio","Parameters")/10)
+    COLS = int(frameWidth)
+    for cnt in contours:
+        if cv2.contourArea(cnt) > MIN_CONTOUR_AREA and cv2.contourArea(cnt) < MAX_CONTOUR_AREA:
+            cnt = cv2.convexHull(cnt)
+            moments = cv2.moments(cnt)
+            com_x = int(moments['m10']/moments['m00'])
+            com_y = int(moments['m01']/moments['m00'])
+            if cv2.pointPolygonTest(points,(com_x,com_y),True) > 0:
+                cnt_list.append([cnt, com_x,com_y])
+                y_list.append(com_y)
+    if y_list is not None:
+        y_list.sort()
+    else:
+        return -1, -1, -1
+    for item in cnt_list:
+        if item[2] == y_list[-1]:
+            angle_deviation = int(math.atan((int(COLS/2) - item[1])/(ROWS - item[2])) * 180 / math.pi)
+            distance_unobstructed = int(math.sqrt(math.pow((int(COLS/2) - item[1]),2) + math.pow((ROWS - item[2]),2)))
+            area_unobstructed = int(cv2.contourArea(item[0]))
+            cv2.drawContours(imgContour,cnt,-1, (0,255,0),5)
+            if Vis:
+                cv2.circle(img,(item[1],item[2]), 3,3)
+                cv2.drawContours(img, [item[0]], 0, (0,255,0),2)
+                cv2.line(img,( int(COLS/2),ROWS), (item[1], item[2]), (0,255,0),1)
+                cv2.putText(img, str(angle_deviation), (item[1] + 10, item[2] + 10),cv2.FONT_HERSHEY_COMPLEX , 1, (255,0,0),2)
+                cv2.putText(img, str(distance_unobstructed), (item[1] - 20, item[2] - 20),cv2.FONT_HERSHEY_COMPLEX , 1, (255,0,0),2)
+
+    return angle_deviation, distance_unobstructed, area_unobstructed
+
 def comnd(ipt):
 	while ipt == []:
 		ipt = input("Please Enter a Command: ")
@@ -76,15 +171,18 @@ def comnd(ipt):
 			print("List of Commands:   ")
 			print("Command:			What it Do:		Arduino Gets:")
 			print("")
-			print("F				  Forward	 	      70")
-			print("B		    	   Back			      66")
-			print("r     			  90deg CW 	         114")
-			print("Q  			     180deg CW 			  82")
-			print("L  				 FWD Line 			  76")
+			print("F			Forward")
+			print("B			Back")
+			print("r			90deg CW")
+			print("Q			180deg CW")
+			print("L			FWD Line")
+			print("T			Balloon Tracking")
+			print("I			IR Sensor Reading")
+			print("S			Stop")
 			ipt = []
 		else:
-			#ipt = list(ipt)
-			#ipt = ipt[1]
+			ipt = list(ipt)
+			ipt = ipt[1]
 			print("Sending:   ",ipt)
 			ser.write(str(ipt).encode('utf-8'))
 			sleep(.5)
@@ -122,7 +220,7 @@ while True:
 		# mask is the masked image of the blured hsv that seeks values of h,s,and v
 		# within the specified bounds
 		mask = cv2.inRange(hsv, lg, ug)
-                
+
 		#print(len(mask))
 
 		#imgGray and Canny are unused currently, haven't figured out if this is
@@ -161,6 +259,47 @@ while True:
 		while ser.in_waiting > 0:
 			line = ser.readline().decode('utf-8').rstrip()
 			print(line)
-		
+
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
+cap.release()
+cv2.destroyAllWindows()
+
+	while ipt == "P":
+	    success, img = cap.read()
+	    print(img)
+	    cropped,h,w  = crop(img)
+	    #cv2.imshow("Cropped: ", cropped)
+	    imgContours = img.copy()
+	    Sens = cv2.getTrackbarPos("Sensetivity","Parameters")/10
+	    points = polygon(int(h),w,Sens) #polygon(int(frameHeight * Sens),frameWidth,Sens)
+	    #print(points)
+	    imgImp = superimpose(cropped, points)
+	    #imgBlur = cv2.GaussianBlur(img, (5,5),1)
+	    imgGray = cv2.cvtColor(imgImp,cv2.COLOR_BGR2GRAY)
+	    imgBlur = cv2.medianBlur(imgGray,3)
+	    imgBlur = cv2.GaussianBlur(imgGray,(5,5),0)
+	    #cv2.imshow("Blur",imgBlur)
+	    threshold1 = cv2.getTrackbarPos("Thresh 1","Parameters")
+	    threshold2 = cv2.getTrackbarPos("Thresh 2","Parameters")
+	    #imgCanny = cv2.Canny(imgGray,threshold1,threshold2)
+	    #imgCanny = cv2.Canny(imgImp,threshold1,threshold2)
+	    imgCanny = cv2.Canny(imgBlur,threshold1,threshold2)
+
+	    cv2.imshow("Canny", imgCanny)
+	    kernel = np.ones((7,7))
+	    imgDil = cv2.dilate(imgCanny,kernel,iterations=1)
+	    imgDil = ~imgDil
+	    cv2.imshow("Dil", imgDil)
+	    contours,hierarchy  = poly_cont(imgDil)
+	    #contours = getContours(imgDil,imgContours)
+	    findpath(cropped,imgContours,points,contours, True)
+	    #findpath(cropped,imgDil,points,contours, True)
+	    #imgStack = stackImages(0.8,([img,cropped],[img,img]))
+	    #cv2.imshow("Image:  ",imgDil)
+	    cv2.imshow("Final",cropped)
+	    #cv2.imshow("Result",imgStack)
+	    if cv2.waitKey(1) & 0xFF == ord('q'):
+	        break
+	cap.release()
+	cv2.destroyAllWindows()
