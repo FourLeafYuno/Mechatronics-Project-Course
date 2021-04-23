a61e6fd7e302d7578b8e4092d32eade72d0df134
Branden <Branden.Adams@colorado.edu>
2021-04-23 17:14:51 -0600

    Recent rev

--- Lab4_2.py
+++ Lab4_2.py
@@ -12,16 +12,16 @@ import math
 os.environ['DISPLAY'] = ':0'
 
 # Frame size in pixels
-frameWidth = 800
-frameHeight = 600
-ARDUINO = False
+frameWidth = 640
+frameHeight = 480
+ARDUINO = True
 
 #Establishes serial connection with the Arduino before the code can be run
 if ARDUINO:
-	if __name__ == '__main__':
-		ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-		#ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-		ser.flush()
+    if __name__ == '__main__':
+        ser = serial.Serial('/dev/ttyUSB1', 115200, timeout=1)
+        #ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        ser.flush()
 
 # This commande activates the video capture
 cap = cv2.VideoCapture(0)
@@ -30,7 +30,7 @@ cap.set(4,frameHeight)
 
 
 def empty(a):
-	pass
+    pass
 
 cv2.namedWindow("Parameters")
 cv2.resizeWindow("Parameters",800,600)
@@ -58,29 +58,29 @@ COLS = int(frameWidth)
 
 #Function to draw contours based on the image and parameters provided
 def getContours(img,imgContour):
-	global cent, area, b
-	contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-	for cnt in contours:
-		area = cv2.contourArea(cnt)
-		if area > cv2.getTrackbarPos("Area Min","Parameters"):
-			cv2.drawContours(imgContour,cnt,-1,(0,255,0),5)
-			pari = cv2.arcLength(cnt,True)
-			approx = cv2.approxPolyDP(cnt,0.02 * pari, True)
-			#print(len(approx))
-			x,y,w,h = cv2.boundingRect(approx)
-			cent = x+(w/2)
-			cv2.rectangle(imgContour, (x,y),(x + w, y + h), (255,0,0),5)
-			cv2.putText(imgContour, "Points: " + str(len(approx)),(x + w + 20, y + 20),cv2.FONT_HERSHEY_COMPLEX, .7, (0,0,255),2)
-			cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),2)
-			b = True
+    global cent, area, b
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > cv2.getTrackbarPos("Area Min","Parameters"):
+            cv2.drawContours(imgContour,cnt,-1,(0,255,0),5)
+            pari = cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt,0.02 * pari, True)
+            #print(len(approx))
+            x,y,w,h = cv2.boundingRect(approx)
+            cent = x+(w/2)
+            cv2.rectangle(imgContour, (x,y),(x + w, y + h), (255,0,0),5)
+            cv2.putText(imgContour, "Points: " + str(len(approx)),(x + w + 20, y + 20),cv2.FONT_HERSHEY_COMPLEX, .7, (0,0,255),2)
+            cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),2)
+            b = True
 
 def crop(img):
     IMAGE_Y_INDEX = 1
     IMAGE_X_INDEX = 0
 
     roi_vector = {'x' : 0, 'y' : 0, 'w' : 0, 'h': 0}
-    roi_vector['w'] = 800
-    roi_vector['h'] = int(600 * (cv2.getTrackbarPos("Frame Ratio","Parameters")/10))
+    roi_vector['w'] = 640
+    roi_vector['h'] = int(480 * (cv2.getTrackbarPos("Frame Ratio","Parameters")/10))
     roi_vector['x'] = 0
     roi_vector['y'] = img.shape[IMAGE_Y_INDEX] - roi_vector['h']
     y_start = roi_vector['y']
@@ -129,7 +129,7 @@ def findpath(img,imgContour,points,contours,Vis):
     area_unobstructed = 0
     MIN_CONTOUR_AREA = cv2.getTrackbarPos("Area Min","Parameters")
     MAX_CONTOUR_AREA = cv2.getTrackbarPos("Area Max","Parameters")
-    rows,cols = 600 ,800
+    rows,cols = 480, 640
 
     if contours is None:
         return -1, -1, -1
@@ -167,150 +167,150 @@ def findpath(img,imgContour,points,contours,Vis):
     return angle_deviation, distance_unobstructed, area_unobstructed
 
 def comnd(ipt):
-	while ipt == []:
-		ipt = input("Please Enter a Command: ")
-		if ipt == "list":
-			print("List of Commands:   ")
-			print("Command:		What it Do:")
-			print("")
-			print("F			Forward")
-			print("B			Back")
-			print("S			Stop")
-			print("R			90deg CW")
-			print("r			90deg CCW")
-			print("L			FWD Line")
-			print("*			Charge Balloon")
-			print("M			Maze Solver")
-			print("I			IR Sensor Reading")
-			print("D			Distance Check [cm]")
-			print("-			Color Check")
-			print("c			CW Large Radius")
-			print("C			CCW Large Radius")
-			print("T			Balloon Tracking")
+    while ipt == []:
+        ipt = input("Please Enter a Command: ")
+        if ipt == "list":
+            print("List of Commands:   ")
+            print("Command:     What it Do:")
+            print("")
+            print("F            Forward")
+            print("B            Back")
+            print("S            Stop")
+            print("R            90deg CW")
+            print("r            90deg CCW")
+            print("L            FWD Line")
+            print("*            Charge Balloon")
+            print("M            Maze Solver")
+            print("I            IR Sensor Reading")
+            print("D            Distance Check [cm]")
+            print("-            Color Check")
+            print("c            CW Large Radius")
+            print("C            CCW Large Radius")
+            print("T            Balloon Tracking")
 
-			print("Q			180deg CW")
+            print("Q            180deg CW")
 
-			ipt = []
-		else:
-			#ipt = list(ipt)
-			#ipt = ipt[1]
-			print("Sending:   ",ipt)
-			if ARDUINO:
-				ser.write(str(ipt).encode('utf-8'))
-				sleep(.5)
-				while ser.in_waiting > 0 and ARDUINO == True:
-					line = ser.readline().decode('utf-8').rstrip()
-					print(line)
-	return ipt
+            ipt = []
+        else:
+            #ipt = list(ipt)
+            #ipt = ipt[1]
+            print("Sending:   ",ipt)
+            if ARDUINO:
+                ser.write(str(ipt).encode('utf-8'))
+                sleep(.5)
+                while ser.in_waiting > 0 and ARDUINO == True:
+                    line = ser.readline().decode('utf-8').rstrip()
+                    print(line)
+    return ipt
 
 
 # Main loop of the script
 
 while True:
-	ipt = []
-	ipt = comnd(ipt)
-	while ipt == "T":
-		_,frame = cap.read()
-		#print(len(frame))
-		imgContours = frame.copy()
-		#print("Inside the Balloon while")
-		# Thresholds for contour stuff (unused as of now I think)
-		threshold1 = cv2.getTrackbarPos("Thresh 1","Parameters")
-		threshold2 = cv2.getTrackbarPos("Thresh 2","Parameters")
+    ipt = []
+    ipt = comnd(ipt)
+    while ipt == "T":
+        _,frame = cap.read()
+        #print(len(frame))
+        imgContours = frame.copy()
+        #print("Inside the Balloon while")
+        # Thresholds for contour stuff (unused as of now I think)
+        threshold1 = cv2.getTrackbarPos("Thresh 1","Parameters")
+        threshold2 = cv2.getTrackbarPos("Thresh 2","Parameters")
 
-		# Higher and lower thresholds of the HSV values
-		lg = np.array([cv2.getTrackbarPos("H_LOW ","Parameters"),cv2.getTrackbarPos("S_LOW ","Parameters"),cv2.getTrackbarPos("V_LOW ","Parameters")])
-		ug = np.array([cv2.getTrackbarPos("H_HIGH ","Parameters"),cv2.getTrackbarPos("S_HIGH ","Parameters"),cv2.getTrackbarPos("V_HIGH ","Parameters")])
-		hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        # Higher and lower thresholds of the HSV values
+        lg = np.array([cv2.getTrackbarPos("H_LOW ","Parameters"),cv2.getTrackbarPos("S_LOW ","Parameters"),cv2.getTrackbarPos("V_LOW ","Parameters")])
+        ug = np.array([cv2.getTrackbarPos("H_HIGH ","Parameters"),cv2.getTrackbarPos("S_HIGH ","Parameters"),cv2.getTrackbarPos("V_HIGH ","Parameters")])
+        hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 
-		imgBlur = cv2.GaussianBlur(hsv,(7,7),1)
-		#testBlur = cv2.GaussianBlur(frame,(7,7),1)
-		# mask is the masked image of the blured hsv that seeks values of h,s,and v
-		# within the specified bounds
-		mask = cv2.inRange(hsv, lg, ug)
+        imgBlur = cv2.GaussianBlur(hsv,(7,7),1)
+        #testBlur = cv2.GaussianBlur(frame,(7,7),1)
+        # mask is the masked image of the blured hsv that seeks values of h,s,and v
+        # within the specified bounds
+        mask = cv2.inRange(hsv, lg, ug)
 
-		#print(len(mask))
+        #print(len(mask))
 
-		#imgGray and Canny are unused currently, haven't figured out if this is
-		#Something we would like to pursue in the future.
-		imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
-		imgCanny = cv2.Canny(imgGray,threshold1,threshold2)
-		kernel = np.ones((5,5))
-		imgDil = cv2.dilate(mask, kernel, iterations = 1)
-		b = False
-		getContours(imgDil, imgContours)
-#		Image outputs for the pure image, the masked image, and the contours
-		cv2.imshow("Frame: ",frame)
-		cv2.imshow("HSV:   ",hsv)
-		cv2.imshow("HSV Blur  ", imgBlur)
-		cv2.imshow("Mask: ",mask)
-		cv2.imshow("imgDil",imgDil)
-		cv2.imshow("Contours: ", imgContours)
-		print(b)
-		#print(area)
-		if (b == True and area < 100000 and Arduino == True):
-			#print(cent)
-			if (cent < 80):
-				ser.write(str(2).encode('utf-8'))
-				#sleep(.4)
-				print("left")
-			if (cent > 510):
-				#ser.write(str(3).encode('utf-8'))
-				print("right")
-				#sleep(.4)
-			if (cent >=  80 and cent <= 510):
-				ser.write(str(1).encode('utf-8'))
-				print("straight")
-				#sleep(.4)
-			ser.write(str(0).encode('utf-8'))
-		# Command to read back what was sent to the Arduino
-		while ser.in_waiting > 0:
-			line = ser.readline().decode('utf-8').rstrip()
-			print(line)
+        #imgGray and Canny are unused currently, haven't figured out if this is
+        #Something we would like to pursue in the future.
+        imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+        imgCanny = cv2.Canny(imgGray,threshold1,threshold2)
+        kernel = np.ones((5,5))
+        imgDil = cv2.dilate(mask, kernel, iterations = 1)
+        b = False
+        getContours(imgDil, imgContours)
+        #Image outputs for the pure image, the masked image, and the contours
+        cv2.imshow("Frame: ",frame)
+        cv2.imshow("HSV:   ",hsv)
+        cv2.imshow("HSV Blur  ", imgBlur)
+        cv2.imshow("Mask: ",mask)
+        cv2.imshow("imgDil",imgDil)
+        cv2.imshow("Contours: ", imgContours)
+        print(b)
+        #print(area)
+        if (b == True and area < 100000 and ARDUINO == True):
+            #print(cent)
+            if (cent < 80):
+                ser.write(str(2).encode('utf-8'))
+                #sleep(.4)
+                print("left")
+            if (cent > 510):
+                #ser.write(str(3).encode('utf-8'))
+                print("right")
+                #sleep(.4)
+            if (cent >=  80 and cent <= 510):
+                ser.write(str(1).encode('utf-8'))
+                print("straight")
+                #sleep(.4)
+            ser.write(str(0).encode('utf-8'))
+        # Command to read back what was sent to the Arduino
+        while ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
 
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-	while ipt == "P":
-		threshold1 = cv2.getTrackbarPos("Thresh 1","Parameters")
-		threshold2 = cv2.getTrackbarPos("Thresh 2","Parameters")
-		lg = np.array([cv2.getTrackbarPos("H_LOW ","Parameters"),cv2.getTrackbarPos("S_LOW ","Parameters"),cv2.getTrackbarPos("V_LOW ","Parameters")])
-		ug = np.array([cv2.getTrackbarPos("H_HIGH ","Parameters"),cv2.getTrackbarPos("S_HIGH ","Parameters"),cv2.getTrackbarPos("V_HIGH ","Parameters")])
-		success, img = cap.read()
-		
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    while ipt == "P":
+        threshold1 = cv2.getTrackbarPos("Thresh 1","Parameters")
+        threshold2 = cv2.getTrackbarPos("Thresh 2","Parameters")
+        lg = np.array([cv2.getTrackbarPos("H_LOW ","Parameters"),cv2.getTrackbarPos("S_LOW ","Parameters"),cv2.getTrackbarPos("V_LOW ","Parameters")])
+        ug = np.array([cv2.getTrackbarPos("H_HIGH ","Parameters"),cv2.getTrackbarPos("S_HIGH ","Parameters"),cv2.getTrackbarPos("V_HIGH ","Parameters")])
+        success, img = cap.read()
+        print("Hello")
+        cropped,h,w  = crop(img)
+        print("Goodbye")
+        imgHSV = cv2.cvtColor(cropped,cv2.COLOR_BGR2HSV)
+        imgMask = cv2.inRange(imgHSV,lg,ug)
+        cv2.imshow("Mask: ", imgMask)
+        imgContours = img.copy()
+        Sens = cv2.getTrackbarPos("Sensetivity","Parameters")/10
+        points = polygon(int(h),w,Sens)
+        imgImp = superimpose(cropped, points)
 
-		cropped,h,w  = crop(img)
-		imgHSV = cv2.cvtColor(cropped,cv2.COLOR_BGR2HSV)
-		imgMask = cv2.inRange(imgHSV,lg,ug)
-		cv2.imshow("Mask: ", imgMask)
-		imgContours = img.copy()
-		Sens = cv2.getTrackbarPos("Sensetivity","Parameters")/10
-		points = polygon(int(h),w,Sens)
-		imgImp = superimpose(cropped, points)
+        imgGray = cv2.cvtColor(imgImp,cv2.COLOR_BGR2GRAY)
+        imgBlur = cv2.medianBlur(imgGray,3)
+        imgBlur = cv2.GaussianBlur(imgGray,(5,5),0)
+        #cv2.imshow("Blur",imgBlur)
 
-		imgGray = cv2.cvtColor(imgImp,cv2.COLOR_BGR2GRAY)
-		imgBlur = cv2.medianBlur(imgGray,3)
-		imgBlur = cv2.GaussianBlur(imgGray,(5,5),0)
-		#cv2.imshow("Blur",imgBlur)
+        imgCanny = cv2.Canny(imgBlur,threshold1,threshold2)
 
-		imgCanny = cv2.Canny(imgBlur,threshold1,threshold2)
-
-		cv2.imshow("Canny", imgCanny)
-		kernel = np.ones((7,7))
-		imgDil = cv2.dilate(imgCanny,kernel,iterations=1)
-		imgDil = ~imgDil
-		cv2.imshow("Dil", imgDil)
-		contours,hierarchy  = poly_cont(imgDil)
-		contours_p = getContours(imgMask,cropped)
-		angle_deviation, distance_unobstructed, area_unobstructed = findpath(cropped,imgContours,points,contours, True)
-		if ARDUINO:
-			ser.write(str(angle_deviation).encode('utf-8'))
-			ser.write(str(distance_unobstructed).encode('utf-8'))
-		#findpath(cropped,imgDil,points,contours, True)
-		#imgStack = stackImages(0.8,([img,cropped],[img,img]))
-		#cv2.imshow("Image:  ",imgDil)
-		cv2.imshow("Final",cropped)
-		#cv2.imshow("Result",imgStack)
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
+        cv2.imshow("Canny", imgCanny)
+        kernel = np.ones((7,7))
+        imgDil = cv2.dilate(imgCanny,kernel,iterations=1)
+        imgDil = ~imgDil
+        cv2.imshow("Dil", imgDil)
+        contours,hierarchy  = poly_cont(imgDil)
+        contours_p = getContours(imgMask,cropped)
+        angle_deviation, distance_unobstructed, area_unobstructed = findpath(cropped,imgContours,points,contours, True)
+        if ARDUINO:
+            ser.write(str(angle_deviation).encode('utf-8'))
+            ser.write(str(distance_unobstructed).encode('utf-8'))
+        #findpath(cropped,imgDil,points,contours, True)
+        #imgStack = stackImages(0.8,([img,cropped],[img,img]))
+        #cv2.imshow("Image:  ",imgDil)
+        cv2.imshow("Final",cropped)
+        #cv2.imshow("Result",imgStack)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 cap.release()
 cv2.destroyAllWindows()
